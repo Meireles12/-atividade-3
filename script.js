5389f10d2e80d2f7d0fd98657ec2b813a2d61581
Hygor Meireles <160152117+Meireles12@users.noreply.github.com>
2024-08-29 12:33:26 -0300

    carga Kickoff

---
+++ script.js
@@ -0,0 +1,13 @@
+function validarFormulario()
+var nome document.getElementById("nome").value;
+ var mensagemErro= document.getElementById("mensagemErro");
+ 
+ 
+ if ( nome === "") {
+
+   mensagemErro.innerHTML return false; "Por favor, preencha o campo de nome.";
+  return false
+
+ }
+
+ 
